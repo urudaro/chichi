07c4923039ab36b1801d2f891fd4cc0d855beb99
Daniel Rodriguez <urudaro@gmail.com>
2020-03-26 00:03:54 +0100

    first nr 3

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  title: 'Theme',
+  description: 'Diversidades',
+  base: '/',
+  dest: 'blog/.vuepress/dist/',
+  theme: 'ououe',
+  themeConfig: {
+    cover: '/pueblo.jpg',
+    logo: '/chichi-t-short.png',
+    backgroundImage: false,
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Posts', link: '/posts/' },
+      { text: 'Tags', link: '/tag/' },
+      { text: 'Categories', link: '/category/' },
+      { text: 'About', link: '/about/' }
+    ],
+    footer: [
+      { text: 'Github', link: 'https://github.com/tolking' }
+    ]
+  },
+  plugins: [
+    // permalink for posts
+    ['blog-multidir', {
+      postsDir: {
+        posts: 'posts/:year/:month/:day/:slug'
+      }
+    }],
+    // add vuepress-plugin-container
+    ['container', {
+      type: 'right',
+      defaultTitle: ''
+    }],
+    ['container', {
+      type: 'tip',
+      before: info => `<div class="tip"><p class="title">${info}</p>`,
+      after: '</div>'
+    }],
+    ['container', {
+      type: 'warning',
+      before: info => `<div class="warning"><p class="title">${info}</p>`,
+      after: '</div>'
+    }],
+    ['container', {
+      type: 'danger',
+      before: info => `<div class="danger"><p class="title">${info}</p>`,
+      after: '</div>'
+    }]
+  ]
+}
---
+++ blog/posts/5-juana-antonio.md
@@ -0,0 +1,12 @@
+---
+title: Juana y Antonio
+image: /amigos/juana-antonio-2.jpg
+date: 2014-07-27
+tags:
+  - fotos
+categories:
+  - Cerro Limón
+  - amigos
+---
+
+![](/amigos/juana-antonio-1.jpg)
